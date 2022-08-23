40eba2276744c1a748ea32d2efbc57d0144cdfba
Rino <rino.ridlo@sirclo.com>
2022-08-23 12:33:11 +0700

    Add bulma

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <a className="navbar-item" href="https://bulma.io">
+                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+                </a>
+
+                <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                </a>
+            </div>
+            <div id="navbarBasicExample" className="navbar-menu">
+                <div className="navbar-start">
+                    <Link className='navbar-item' to="/">Home</Link>
+                    <Link className='navbar-item' to="/category">Category</Link>
+                </div>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../App.css';
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Selamat datang Rino dari Pacitan</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Home
